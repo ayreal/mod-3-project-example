f1d17957369e29e42c28fa8f3be39f9844ec9712
Ariel Herman <aeh.herman@gmail.com>
2017-11-11 14:42:10 -0500

    fix CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'example.com'
+    origins '*'
+#  Later on, we can change this to only allow requests from the address of the frontend repo––localhost:8000 for example.
+
 
     resource '*',
       headers: :any,
